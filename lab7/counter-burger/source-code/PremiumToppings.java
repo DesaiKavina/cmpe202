5004062758b5e0744a16a600df4280daf4cc359c
DesaiKavina <kavina.desai@sjsu.edu>
2018-10-20 20:48:17 -0700

    Lab7 source code and class diagram

---
+++ lab7/counter-burger/source-code/PremiumToppings.java
@@ -0,0 +1,37 @@
+
+/**
+ * Class for adding PremiumToppings.
+ */
+public class PremiumToppings extends LeafDecorator
+{
+    private String[] options ;
+    
+    public PremiumToppings( String d )
+    {
+        super(d) ;
+    }
+    
+    // premium toppings - all for $1.00 except Marinated Tomatoes
+    public void setOptions( String[] options )
+    {
+        this.options = options ;
+        for(int i=0; i<options.length; i++)
+        {
+            if( options[i].equals("Marinated Tomatoes") )
+                this.price += 3.00;
+            else
+                this.price += 1.00;
+        }
+    }
+    
+    public String getDescription() 
+    {
+        String desc = "   " ;
+        for ( int i = 0; i<options.length; i++ )
+        {
+            if (i>0) desc += " + " + options[i] ;
+            else desc = options[i] ;
+        }        
+        return desc ;
+    }
+}
